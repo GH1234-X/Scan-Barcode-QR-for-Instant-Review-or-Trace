53a353456028c2ece695ab73919e5f35ae15f75a
Arman2337 <23it119@charusat.edu.in>
2025-06-15 11:25:07 +0530

    frontend add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="text-gray-500 dark:text-gray-400"> Hello All</div>
+    </>
+  )
+}
+
+export default App
