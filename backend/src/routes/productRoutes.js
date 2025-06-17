c3830186120878c1f9e759af2551dcd41b24afeb
Arman2337 <23it119@charusat.edu.in>
2025-06-17 13:03:10 +0530

    update productRoutes.js

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getAllProducts,getProductByCode, createProduct } from '../controllers/productController.js';
+import { getAllProducts,
+         getProductByCode, 
+         createProduct,
+         updateProduct,   // <-- Add this
+        deleteProduct,  } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
 router.get('/:code', getProductByCode);     // GET /api/products/:code
 router.post('/', createProduct);            // POST /api/products
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
